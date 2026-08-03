f39a2ad41eced832f4b309fc1e71f44addc5fba0
Gmati Ayoub <gmtayoyoub15@gmail.com>
2026-08-03 10:42:23 +0200

    chatbot body et footer teeminer

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,24 @@ const App = () => {
           </div>
           <button className="material-symbols-outlined">keyboard_arrow_down</button>
         </div>
+        {/* Chatbot body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">hey there! How can I help you today?</p>  
+          </div>
+          <div className="message user-message">
+            <p className="message-text">Lorem ipsum dolor sit amet consectetur adipiscing.</p>  
+          </div>
+        </div>
+
+        {/* Chatbot footer */}
+        <div className="chat-footer">
+          <form action="#" className="chat-form">
+            <input type="text" placeholder="Type your message..." className="message-input" required />
+            <button className="matrial-symbols-rounded">arrow_upward</button>
+          </form>
+        </div>
       </div>
     </div>
   );

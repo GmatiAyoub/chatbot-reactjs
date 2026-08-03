34892fd7ccd76a10b8a80bccdb44741ab6c6b4a3
Gmati Ayoub <gmtayoyoub15@gmail.com>
2026-08-03 10:32:02 +0200

    chatbt header complet

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ const App = () => {
   return (
     <div className="container">
       <div className="chatbot-popup">
+        {/* Chatbot header */}
         <div className="chat-header">
           <div className="header-info">
             <ChatbotIcon />
             <h2 className="logo-text">chatbot</h2>
           </div>
+          <button className="material-symbols-outlined">keyboard_arrow_down</button>
         </div>
       </div>
     </div>

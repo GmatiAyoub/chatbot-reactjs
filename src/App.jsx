74978f9f504f49ebf3333800f63d980d604b6a1c
Gmati Ayoub <gmtayoyoub15@gmail.com>
2026-08-03 10:24:20 +0200

    add chatbot icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import ChatbotIcon from './components/ChatbotIcon';
 const App = () => {
   return (
     <div className="container">
       <div className="chatbot-popup">
         <div className="chat-header">
           <div className="header-info">
+            <ChatbotIcon />
             <h2 className="logo-text">chatbot</h2>
           </div>
         </div>
